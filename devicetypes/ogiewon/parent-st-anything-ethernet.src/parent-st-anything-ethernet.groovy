cafba1378c2dcd4fbe4e270ba1841440b1b7d50e
philiprehberger <me@philiprehberger.com>
2019-09-27 14:41:25 -0600

    Update parent-st-anything-ethernet.groovy

--- devicetypes/ogiewon/parent-st-anything-ethernet.src/parent-st-anything-ethernet.groovy
+++ devicetypes/ogiewon/parent-st-anything-ethernet.src/parent-st-anything-ethernet.groovy
@@ -1,7 +1,7 @@
 /**
  *  Parent_ST_Anything_Ethernet.groovy
  *
- *  Copyright 2017 Dan G Ogorchock 
+ *  Copyright 2017 Dan G Ogorchock  
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
